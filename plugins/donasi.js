6b8ae652f1a663ebb347c2e3d78ad4d3c00c74fd
Aron <86765212+Chifuy@users.noreply.github.com>
2022-06-24 22:46:04 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,11 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
-╰────
+*────── 「 DONATE 」 ──────*
+ ❏ *Dana : 0857-4754-3536*
+ ❏ *Gopay : 0857-4754-3536*
+ ❏ *Ovo : 0857-4754-3536*
+ ❏ *Indosat : 0857-4754-3536*
+ ❏ *Smartfren: 0882-3273-3050*
+ ❏ *Tri: 0895-3846-53792*
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
