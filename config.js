0f7d1927b8b90bace6009d5b2b6f2548e14ff817
Aron <86765212+Chifuy@users.noreply.github.com>
2022-06-25 00:41:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -35,9 +35,29 @@ global.APIKeys = { // APIKey Here
   'https://x-restapi.herokuapp.com': 'BETA'
 }
 
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+
+const spack = fs.readFileSync("lib/exif.json")
+const stickerpack = JSON.parse(spack)
+if (stickerpack.spackname == '') {
+  var sticker_name = 'Origami-Bot'
+  var sticker_author = 'Aron'
+} else {
+  var sticker_name = stickerpack.spackname
+  var sticker_author = stickerpack.sauthor
+}
+
+const file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
+})
+
 // Sticker WM
-global.packname = 'Origami-Bot'
-global.author = 'Aron'
+global.packname = sticker_name
+global.author = sticker_author
 global.wm = '© Origami-Bot'
 global.stiker_wait = 'Loading...'
 
