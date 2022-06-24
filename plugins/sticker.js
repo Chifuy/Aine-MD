17b448532eed4fb70e4e64c14068325a26805726
Aron <86765212+Chifuy@users.noreply.github.com>
2022-06-25 00:39:52 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,39 +1,39 @@
-const { sticker1, sticker5 } = require('../lib/sticker')
-
-let handler = async (m, { conn }) => {
-    let stiker = false
-    try {
-        let q = m.quoted ? m.quoted : m
-        let mime = (q.msg || q).mimetype || ''
-        if (/webp/.test(mime)) {
-            let img = await q.download()
-            if (!img) throw `reply sticker with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (/image/.test(mime)) {
-            let img = await q.download()
-            if (!img) throw `reply image with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) return m.reply('max is 10 seconds!')
-            let img = await q.download()
-            if (!img) throw `reply video with command s`
-            stiker = await sticker5(img, false, packname, author)
-        } else if (m.quoted.text) {
-            if (isUrl(m.quoted.text)) stiker = await sticker(false, m.quoted.text, packname, author)
-            else throw 'URL is not valid! end with jpg/gif/png'
-        }
-    } catch (e) {
-        throw e
+const { sticker } = require('../lib/sticker')
+const uploadFile = require('../lib/uploadFile')
+const uploadImage = require('../lib/uploadImage')
+let { webp2png } = require('../lib/webp2mp4')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let stiker = false
+  try {
+    let q = m.quoted ? m.quoted : m
+    let mime = (q.msg || q).mimetype || ''
+    if (/webp|image|video/g.test(mime)) {
+      if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
+      let img = await q.download()
+      if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
+      let out
+      try {
+        if (/webp/g.test(mime)) out = await webp2png(img)
+        else if (/image/g.test(mime)) out = await uploadImage(img)
+        else if (/video/g.test(mime)) out = await uploadFile(img)
+        if (!isUrl(out)) out = await uploadImage(img)
+        stiker = await sticker(false, out, global.packname, global.author)
+      } catch (e) {
+        console.error(e)
+        if (!stiker) stiker = await sticker(img, false, global.packname, global.author)
+      }
+    } else if (args[0]) {
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
+      else return m.reply('URL tidak valid!')
     }
-    finally {
-        if (stiker) {
-          m.reply(stiker_wait)
-          await conn.sendFile(m.chat, stiker, 'stiker.webp', '', m, 0, { contextInfo: { forwardingScore: 999, isForwarded: true } })
-        }
-        else {
-            throw 0
-        }
-    }
+  } catch (e) {
+    console.error(e)
+    if (!stiker) stiker = e
+  } finally {
+    if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    else throw 'Conversion failed'
+  }
+}
 handler.help = ['stiker (caption|reply)', 'stiker <url>']
 handler.tags = ['sticker']
 handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
