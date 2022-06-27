54f93365c49bf9d3755bb169bbbd22919b8ba033
Aron <86765212+Chifuy@users.noreply.github.com>
2022-06-27 12:34:39 +0700

    Update tebakkata.js

--- plugins/tebakkata.js
+++ plugins/tebakkata.js
@@ -31,6 +31,6 @@ handler.help = ['tebakkata']
 handler.tags = ['game']
 handler.command = /^tebakkata/i
 handler.limit = true
-handler.group = true
+handler.group = false
 
 module.exports = handler
