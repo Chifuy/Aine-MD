74c456c42cce5115bf8986642de4bee787907635
Aron <86765212+Chifuy@users.noreply.github.com>
2022-06-29 05:30:37 +0700

    Create anonyButton.js

---
+++ plugins/anonyButton.js
@@ -0,0 +1,16 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix }) => conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'ANONYMOUS CHAT')).buffer(), `
+Hay, ${conn.getName(m.sender)}👋🏻
+Welcome to Anonymous Chat!
+
+Ketik /start untuk memulai
+Ketik /next untuk skip/lanjut
+Ketik /leave untuk keluar/stop
+Ketik /sendkontak untuk mengirim kontak
+`.trim(), 'Anonymous Chat\n© Origami-Bot', null, [['/start', '!start'], ['/next',  '!next'], ['/leave', '!leave']], m)
+handler.help = ['anonymous']
+handler.tags = ['main']
+handler.command = /^anony(mous|chat)$/i
+handler.private = true
+
+module.exports = handler
